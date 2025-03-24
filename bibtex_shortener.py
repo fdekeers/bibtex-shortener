c8a176877fd484ab2024ebb1fa4ff5acc35841e7
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2025-03-24 16:26:27 +0100

    Reduce file open scope. Use new variable for each entry.

--- bibtex_shortener.py
+++ bibtex_shortener.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
 """
-Shorten a bibtex file by replacing author lists of more than 3 authors with \"et al.\"
+Shorten a bibtex file.
+- Replace "online" entries by only their URL
+- Remove optional fields (pages, address)
+- Remove notes for entries other than `misc`
+- Replace author lists of more than 3 authors with \"et al.\"
 """
 
 import os
 from pathlib import Path
 import argparse
+from copy import deepcopy
 import bibtexparser
 
 
@@ -21,7 +26,7 @@ if __name__ == "__main__":
 
     ## ARGUMENT PARSING ##
     parser = argparse.ArgumentParser(
-        description="Shorten a bibtex file by replacing author lists of more than 3 authors with \"et al.\"",
+        description="Shorten a bibtex file.",
         prog=script_name
     )
     # Positional argument: path to the input bibtex file
@@ -33,33 +38,53 @@ if __name__ == "__main__":
     output_path = args.input.replace(".bib", "_short.bib")
 
 
-    ## BIBTEX FILE LOGIC ##
+    ### BIBTEX FILE LOGIC ###
+
+
+    # Load bibtex file
+    bib_db = None
     with open(args.input, "r") as bibfile:
         bib_db = bibtexparser.load(bibfile)
 
-        for entry in bib_db.entries:
 
-            # Remove unnecessary fields
-            for field in removable_fields:
-                if field in entry:
-                    del entry[field]
+    # Process entries
+    entries_new = []
+    for entry in bib_db.entries:
 
-            # Remove unnecessary notes for entries other than `misc`
-            entry_type = entry.get("ENTRYTYPE", "misc")
-            if entry_type != "misc" and "note" in entry:
-                del entry["note"]
+        entry_new = deepcopy(entry)
+        entry_type = entry_new.get("ENTRYTYPE", "misc")
 
-            # Shorten author list
-            authors = entry.get("author", "")
-            n_authors = authors.count(",")
-            if n_authors > 2:
-                i = authors.find(" and")
-                new_authors = authors[:i] if i != -1 else authors
-                spl = new_authors.split(", ")
-                spl[0] = f"{spl[0]} \\textit{{et al.}}"
-                new_authors = f"{spl[0]}, {spl[1]}"
-                entry["author"] = new_authors
+        # Remove unnecessary fields
+        for field in removable_fields:
+            if field in entry_new:
+                del entry_new[field]
+
+        # Remove unnecessary notes for entries other than `misc`
+        if entry_type != "misc" and "note" in entry_new:
+            del entry_new["note"]
+
+        # Replace "online" entries by only their URL
+        if entry_type == "online":
+            url = entry_new.get("url", None)
+            if url is not None:
+                entry_new = {"note": url}
+
+        # Shorten author list
+        authors = entry_new.get("author", "")
+        n_authors = authors.count(",")
+        if n_authors > 2:
+            i = authors.find(" and")
+            new_authors = authors[:i] if i != -1 else authors
+            split = new_authors.split(", ")
+            split[0] = f"{split[0]} \\textit{{et al.}}"
+            new_authors = f"{split[0]}, {split[1]}"
+            entry_new["author"] = new_authors
         
-        # Write output bibtex file
-        with open(output_path, "w") as output:
-            bibtexparser.dump(bib_db, output)
+        # Append new entry
+        entries_new.append(entry_new)
+    
+
+    # Write output bibtex file
+    bib_db.entries = entries_new
+    with open(output_path, "w") as output:
+        bibtexparser.dump(bib_db, output)
